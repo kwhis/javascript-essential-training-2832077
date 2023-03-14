96367af9f2ad5d121c03e7ae04c29e91070fd857
Kels <116579655+kwhis@users.noreply.github.com>
2023-03-14 13:45:37 -0400

    Build cat and dog objects with functions to feed/water them

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,40 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const cat = {
+  name: "Small Fry",
+  breed: "Domestic Long Hair",
+  furColor: "black",
+  eyeColor: "green",
+  hungry: true,
+  thirsty: true,
+  takeCare: function (giveFood, giveWater) {
+    this.hungry = !giveFood;
+    this.thirsty = !giveWater;
+  },
+};
+
+const dog = {
+  name: "Kobi",
+  breed: "Pom-chi",
+  furColor: "black/white",
+  eyeColor: "black",
+  hungry: true,
+  thirsty: false,
+  takeCare: function (giveFood, giveWater) {
+    this.hungry = !giveFood;
+    this.thirsty = !giveWater;
+  },
+};
+
+console.log("Cat object:", cat);
+console.log("Dog object:", dog);
+
+cat.takeCare(true, true);
+console.log("Give cat food and water.");
+dog.takeCare(true, false);
+console.log("Give dog food only.");
+
+console.log("Cat object:", cat);
+console.log("Dog object:", dog);
