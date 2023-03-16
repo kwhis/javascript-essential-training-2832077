737bc84209b9553fc0d0462dd03cd1ebc48deba6
Kels <116579655+kwhis@users.noreply.github.com>
2023-03-15 20:50:30 -0400

    Traverse the dom using js

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -1,4 +1,23 @@
 /**
  * Note: This file is intentionally empty.
+ *
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+document.querySelector("body").style.backgroundColor = "black";
+
+document.querySelector(".maincontent").style.backgroundColor = "orange";
+
+document.querySelector(".siteheader").style.backgroundColor = "green";
+
+document.querySelector("header div").style.backgroundColor = "pink";
+
+document.querySelector("h2").style.backgroundColor = "blue";
+
+document.querySelector(".page-header").style.color = "white";
+
+document.querySelector(".sitefooter").style.backgroundColor = "orange";
+
+document
+  .querySelectorAll("ul li span")
+  .forEach((item) => (item.style.color = "red"));
