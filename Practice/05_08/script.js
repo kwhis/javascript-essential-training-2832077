11e4ae5ac38500ae687f79245d7b9730fd088b9d
Kels <116579655+kwhis@users.noreply.github.com>
2023-03-15 22:03:51 -0400

    Modify class/attribute/style

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,24 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+document
+  .querySelectorAll(".backpack__image")
+  .forEach((item) => item.classList.add("new-class", "another-class"));
+
+document
+  .querySelectorAll(".backpack__image")
+  .forEach((item) => item.classList.remove("another-class"));
+
+document.querySelector("img").setAttribute("alt", "Clipart of gray backpack");
+
+console.log(document.querySelector("img").getAttribute("alt"));
+
+document
+  .querySelector("img")
+  .setAttribute("alt", "Clipart image of a gray backpack");
+
+console.log(document.querySelector("img").getAttribute("alt"));
+
+document.querySelector(".site-title").style.color = "rebeccapurple";
+
+console.log(document.querySelector(".site-description").style);
